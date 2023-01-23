79f4019a11ccd935b72e0d26866ff0782fabd62d
tessc14 <tessc14@gmail.com>
2023-01-23 13:11:07 +0000

    add flag and fun fact jsons

---
+++ fun-facts.js
@@ -0,0 +1,29 @@
+const funFacts = [
+    {"id" : 1, "fact" : "The highest capital city in the world is La Paz, Bolivia, which sits at an elevation of over 11,000 feet above sea level."},
+    {"id" : 2, "fact" : "The longest coastline of any country belongs to Canada, stretching over 202,080 km."},
+    {"id" : 3, "fact" : "The country with the smallest population is Vatican City, with a population of around 800 people."},
+    {"id" : 4, "fact" :"The  most populous country in the world is China, with over 1.4 billion people."},    
+    {"id" : 5, "fact" : "The largest desert in the world, the Sahara, is located in Africa."},   
+    {"id" : 6, "fact" : "The country with the most time zones is France, with 12."},   
+    {"id" : 7, "fact" : "The longest river in the world, the Nile, flows through Egypt."},    
+    {"id" : 8, "fact" : "The country with the most languages spoken is Papua New Guinea, with over 800 different languages."},    
+    {"id" : 9, "fact" : "The largest island in the world is Greenland, which is located in the Arctic."},    
+    {"id" : 10, "fact" : "The country with the most lakes is Finland, with over 180,000 lakes."},    
+    {"id" : 11, "fact" : "The country with the most airports is the United States, with over 13,000 airports."},    
+    {"id" : 12, "fact" : "The highest waterfall in the world is Angel Falls, which is located in Venezuela."},    
+    {"id" : 13, "fact" : "The deepest lake in the world is Lake Baikal, which is located in Russia."},    
+    {"id" : 14, "fact" : "The longest railway line in the world is the Trans-Siberian Railway, which runs from Moscow to Vladivostok in Russia."},    
+    {"id" : 15, "fact" : "The largest volcano in the world is Mauna Loa in Hawaii, which is also the largest mountain on Earth when measured from base to summit."},    
+    {"id" : 16, "fact" : "The deepest canyon in the world is the Cotahuasi Canyon in Peru which is deeper than the Grand Canyon in the United States"},    
+    {"id" : 17, "fact" : "The country with the most neighbors is China, which shares borders with 14 different countries."},    
+    {"id" : 18, "fact" : "The longest border between two countries is the U.S.-Canada border, stretching over 8,890 km."},     
+    {"id" : 19, "fact" : "The highest capital city in the world is La Paz, Bolivia, which sits at an elevation of over 11,000 feet above sea level."},     
+    {"id" : 20, "fact" : "The largest lake in the world by area is the Caspian Sea, which is bordered by five countries: Russia, Kazakhstan, Turkmenistan, Iran, and Azerbaijan."},     
+    {"id" : 21, "fact" : "The lowest point on land is the Dead Sea, which is located between Jordan and Israel and sits at an elevation of -413 meters (-1,349 ft) below sea level."},     
+    {"id" : 22, "fact" : "The largest country in the world by area is Russia, spanning over 17 million square kilometers (6.612 million square miles)."},     
+    {"id" : 23, "fact" : "The largest waterfall by volume is the Iguazu Falls, located on the border of Argentina and Brazil."},     
+    {"id" : 24, "fact" : "The highest mountain peak in the world is Mount Everest, which is located in the Himalayas on the border of Nepal and Tibet, and stands at 29,029 feet (8,848 meters) above sea level."},     
+    {"id" : 25, "fact" : "The longest river in the world to flow entirely within one country is the Yangtze River in China, stretching over 6,300 km (3,915 miles)."}    
+]
+
+module.exports = funFacts
