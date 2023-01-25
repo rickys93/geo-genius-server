3e91dde427beb3f95d4ebb338c1d377bfef3fd76
tessc14 <tessc14@gmail.com>
2023-01-25 10:12:18 +0000

    edit flag hints

--- database/flags.js
+++ database/flags.js
@@ -3,96 +3,96 @@ const flags = [
         id: 1,
         countryName: "Japan",
         flagFile: "./flag-images/jp.png",
-        interestingFact: "Japan consists of 6,852 islands",
+        interestingFact: "This country consists of 6,852 islands",
     },
     {
         id: 2,
         countryName: "Belgium",
         flagFile: "./flag-images/be.png",
         interestingFact:
-            "Belgium produces more than 220,000 tons of chocolate per annum",
+            "This country produces more than 220,000 tons of chocolate per annum",
     },
     {
         id: 3,
         countryName: "Argentina",
         flagFile: "./flag-images/ar.png",
         interestingFact:
-            "Argentina is home to both the highest and lowest points of the Southern Hemisphere",
+            "This country is home to both the highest and lowest points of the Southern Hemisphere",
     },
     {
         id: 4,
         countryName: "Chile",
         flagFile: "./flag-images/cl.png",
         interestingFact:
-            "The northern part of the country is home to the driest desert in the world, Atacama",
+            "The northern part of this country is home to the driest desert in the world, Atacama",
     },
     {
         id: 5,
         countryName: "Norway",
         flagFile: "./flag-images/no.png",
-        interestingFact: "Norway introduced salmon sushi to Japan",
+        interestingFact: "This country introduced salmon sushi to Japan",
     },
     {
         id: 6,
         countryName: "Somalia",
         flagFile: "./flag-images/so.png",
-        interestingFact: "Somalia has the longest coastline in mainland Africa",
+        interestingFact: "This country has the longest coastline in mainland Africa",
     },
     {
         id: 7,
         countryName: "Vietnam",
         flagFile: "./flag-images/vn.png",
-        interestingFact: "Vietnam has the world's largest cave",
+        interestingFact: "This country has the world's largest cave",
     },
     {
         id: 8,
         countryName: "Turkey",
         flagFile: "./flag-images/tr.png",
-        interestingFact: "The story of Santa Claus originated in Turkey",
+        interestingFact: "The story of Santa Claus originated in this country",
     },
     {
         id: 9,
         countryName: "Sweden",
         flagFile: "./flag-images/se.png",
-        interestingFact: "More than half of Sweden is covered in forest",
+        interestingFact: "More than half of this country is covered in forest",
     },
     {
         id: 10,
         countryName: "Poland",
         flagFile: "./flag-images/pl.png",
         interestingFact:
-            "In Poland, everyone has a special holiday for their name",
+            "In this country, everyone has a special holiday for their name",
     },
     {
         id: 11,
         countryName: "New Zealand",
         flagFile: "./flag-images/nz.png",
         interestingFact:
-            "There are five sheep for every resident in New Zealand",
+            "There are five sheep for every resident in this country",
     },
     {
         id: 12,
         countryName: "Mexico",
         flagFile: "./flag-images/mx.png",
-        interestingFact: "69 different languages are spoken in Mexico.",
+        interestingFact: "69 different languages are spoken in this country.",
     },
     {
         id: 13,
         countryName: "South Korea",
         flagFile: "./flag-images/kr.png",
-        interestingFact: "South Koreans are one year old when born",
+        interestingFact: "In this country, babies are one year old when born",
     },
     {
         id: 14,
         countryName: "Jamaica",
         flagFile: "./flag-images/jm.png",
-        interestingFact: "Jamaica produces the most music per capita. ",
+        interestingFact: "This country produces the most music per capita. ",
     },
     {
         id: 15,
         countryName: "India",
         flagFile: "./flag-images/in.png",
-        interestingFact: "India is the wettest inhabited place on Earth.",
+        interestingFact: "This country is the wettest inhabited place on Earth.",
     },
 ];
 
